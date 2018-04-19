3d7e34a255d12a0728fa4da86c4d24305c22d6b3
Sergei Maertens <sergei@maykinmedia.nl>
2018-04-19 16:19:14 +0200

    Fix SQL for Postgres

--- django_comments/migrations/0004_auto_20160722_1840.py
+++ django_comments/migrations/0004_auto_20160722_1840.py
@@ -10,10 +10,23 @@ class Migration(migrations.Migration):
         ('django_comments', '0003_add_submit_date_index'),
     ]
 
-    operations = [
+    state_operations = [
         migrations.operations.AlterField(
             model_name='comment',
             name='object_pk',
             field=models.PositiveIntegerField(verbose_name="object ID")
+        )
+    ]
+
+    db_operations = [
+        migrations.RunSQL([
+            'ALTER TABLE "django_comments" ALTER COLUMN "object_pk" TYPE integer USING object_pk::integer;',
+            'ALTER TABLE "django_comments" ADD CONSTRAINT "django_comments_object_pk_5c9578d83bf85384_check" CHECK ("object_pk" >= 0);',
+        ])
+    ]
+
+    operations = [
+        migrations.SeparateDatabaseAndState(
+            database_operations=db_operations, state_operations=state_operations
         )
     ]
